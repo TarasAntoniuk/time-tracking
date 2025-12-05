9b35261b2303e2648416b1848cd9391e89c3e865
taras <bronya2004@gmail.com>
2025-12-05 21:04:24 +0100

    fix: update CORS configuration to support Swagger UI requests

--- src/main/java/com/tarasantoniuk/time_tracking/config/WebConfig.java
+++ src/main/java/com/tarasantoniuk/time_tracking/config/WebConfig.java
@@ -5,15 +5,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://timetracking.tarasantoniuk.com",
+                        "http://localhost:8081"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
